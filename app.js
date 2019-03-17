2c83366a3850b96ee500835a8d6e83594aaae546
t-kowalewski <wkowalewski@outlook.com>
2019-03-17 18:28:30 +0100

    add 'play again' and random correct num gen

--- app.js
+++ app.js
@@ -7,26 +7,32 @@
 
 // Game Values
 let min = 1,
-    max = 10,
-    correctNum = 2,
-    guessesLeft = 3;
+  max = 10,
+  correctNum = getRandomNum(min, max),
+  guessesLeft = 3;
 
 // UI Elements
 const game_ui = document.querySelector('#game'),
-      minNum = document.querySelector('.min-num'),
-      maxNum = document.querySelector('.max-num'),
-      guessInput = document.querySelector('#guess-input'),
-      guessBtn = document.querySelector('#guess-btn'),
-      gameMessage = document.querySelector('.message');
+  minNum = document.querySelector('.min-num'),
+  maxNum = document.querySelector('.max-num'),
+  guessInput = document.querySelector('#guess-input'),
+  guessBtn = document.querySelector('#guess-btn'),
+  gameMessage = document.querySelector('.message');
 
 // Assign UI min & max
 minNum.textContent = min;
 maxNum.textContent = max;
 
-// Event listener for button
-guessBtn.addEventListener('click', function() {
+// Play Again - Event Listener
+game_ui.addEventListener('mousedown', function (e) {
+  if (e.target.className === 'play-again') {
+    window.location.reload();
+  };
+});
+
+// Submit - Event Listener
+guessBtn.addEventListener('click', function () {
   let guess = guessInput.valueAsNumber;
-  console.log(guess);
   // Validate input
   if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
@@ -39,7 +45,7 @@ guessBtn.addEventListener('click', function() {
     }
     else {
       guessesLeft -= 1;
-  
+
       // Wrong guess - Game Over
       if (guessesLeft === 0) {
         gameOver(false, `Wrong guess - Game Over. Correct number was: ${correctNum}`);
@@ -67,10 +73,19 @@ function gameOver(won, msg) {
   guessInput.style.borderColor = color;
   // Message & color
   setMessage(msg, color);
+
+  // Play Again?
+  guessBtn.textContent = 'Play Again';
+  guessBtn.className += 'play-again';
 }
 
 // Set message - function
 function setMessage(msg, color) {
   gameMessage.style.color = color;
   gameMessage.textContent = msg;
+}
+
+// Generate random correct number - Function
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * ((max - min) + 1) + min);
 }
